129885b6a79892305595ee8ecc5e65caae745ecb
Jose Alejandro Montenegro Monzon <machetemontenegro@hotmail.com>
2019-11-12 14:28:01 -0600

    Se creó el Proyecto

---
+++ LAB6_1229918_1251518_MVC/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace LAB6_1229918_1251518_MVC
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ LAB6_1229918_1251518_MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LAB6_1229918_1251518_MVC.MvcApplication" Language="C#" %>
