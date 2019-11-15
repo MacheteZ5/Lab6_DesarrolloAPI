32a0e21a9a64236e58af059450a9c40c89e0dd56
feroliv4z <feroliv4z@gmail.com>
2019-11-15 11:02:49 -0600

    correción de generacion token, se establecio recibir la llave como parametro y se cambio el proyecto de consola a una bilioteca de clases

--- LAB6_1251518_1229918_API/Controllers/TokenController.cs
+++ LAB6_1251518_1229918_API/Controllers/TokenController.cs
@@ -1,22 +1,19 @@
-﻿using System;
-using System.IdentityModel.Tokens.Jwt;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.IdentityModel.Tokens;
-using System.Security.Claims;
-using System.Web.Script.Serialization;
-using protoken;
+﻿using Microsoft.AspNetCore.Mvc;
 namespace LAB6_1251518_1229918_API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class TokenController : ControllerBase
     {
-        [HttpGet]
-        public ActionResult GetToken()
+        [HttpPost]
+        public ActionResult SetToken(string key)
         {
-            var token = protoken.TokenGenerator.generator();
+            return RedirectToAction("GetToken", new { key = key });
+        }
+        [HttpGet]
+        public ActionResult GetToken(string key)
+        {
+            var token = tokenGen.tokenGenerator.generator(key);
             return Ok(token);
         }
     }
--- tokenGen/tokenGenerator.cs
+++ tokenGen/tokenGenerator.cs
@@ -1,37 +1,32 @@
 ﻿using Microsoft.IdentityModel.Tokens;
-using Nancy.Json;
 using System;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace protoken
+namespace tokenGen
 {
-    public class TokenGenerator
+    public class tokenGenerator
     {
-        public static void Main(string[] args)
-        {
-
-        }
-        public static async Task <string> generator()
+        public static async Task<string> generator(string privateKey)
         {
             var user = new User { UserId = 1, EmailAddress = "fernando@sendpizza.com", FirstName = "Fernando", LastName = "Oliva" };
-            var json = new JavaScriptSerializer().Serialize(user);
+            var userJson = Newtonsoft.Json.JsonConvert.SerializeObject(user);
             var issuer = "Machete.com/remitente";
             var authority = "MacheteMontenegro";
             //la llave privada debe contener 256 caracteres
-            var privateKey = "J6k2eVCTXDp5b97u6gNH5GaaqHDxCmzz2wv3PRPFRsuW2UavK8LGPRauC4VSeaetKTMtVmVzAC8fh8Psvp8PFybEvpYnULHfRpM8TA2an7GFehrLLvawVJdSRqh2unCnWehhh2SJMMg5bktRRapA8EGSgQUV8TCafqdSEHNWnGXTjjsMEjUpaxcADDNZLSYPMyPSfp6qe5LMcd5S9bXH97KeeMGyZTS2U8gp3LGk2kH4J4F3fsytfpe9H9qKwgjb";
-            var userJson = new JavaScriptSerializer().Serialize(user);
             var createJwt = await CreateJWTAsync(user, issuer, authority, privateKey, userJson);
             return createJwt;
         }
-        public static async Task<string> CreateJWTAsync(User user, string issuer, string authority, string symSec, string userJson)       
+        public static async Task<string> CreateJWTAsync(User user, string issuer, string authority, string symSec, string userJson)
         {
             var tokenHandler = new JwtSecurityTokenHandler();
-            var claims = await CreateClaimsIdentities("Datos Usuario en Json "+userJson);
 
+
+            var claims = await CreateClaimsIdentities(userJson);
             var token = tokenHandler.CreateJwtSecurityToken(
+
                 issuer: issuer,
                 audience: authority,
                 subject: claims,
@@ -49,16 +44,17 @@ namespace protoken
         public static Task<ClaimsIdentity> CreateClaimsIdentities(string userJson)
         {
             ClaimsIdentity claimsIdentity = new ClaimsIdentity();
-            claimsIdentity.AddClaim(new Claim(ClaimTypes.UserData, userJson));
+            claimsIdentity.AddClaim(new Claim(ClaimTypes.Name, userJson));
             return Task.FromResult(claimsIdentity);
         }
-        public class User
-        {
-            public int UserId { get; set; }
-            public string EmailAddress { get; set; }
-            public string FullName { get; set; }
-            public string FirstName { get; set; }
-            public string LastName { get; set; }
-        }
+        
+    }
+    public class User
+    {
+        public int UserId { get; set; }
+        public string EmailAddress { get; set; }
+        public string FullName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
     }
 }
