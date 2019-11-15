d1c59fdecdaf80ddd782bc43c66b1e3134d3a3e1
feroliv4z <feroliv4z@gmail.com>
2019-11-14 20:26:03 -0600

    añadiendo generacion correcta del token, se añadio un proyecto de consola a la solución para ello

--- LAB6_1251518_1229918_API/Controllers/TokenController.cs
+++ LAB6_1251518_1229918_API/Controllers/TokenController.cs
@@ -1,13 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using System.Security.Claims;
+using System.Web.Script.Serialization;
+using protoken;
 namespace LAB6_1251518_1229918_API.Controllers
 {
     [Route("api/[controller]")]
@@ -17,18 +16,8 @@ namespace LAB6_1251518_1229918_API.Controllers
         [HttpGet]
         public ActionResult GetToken()
         {
-            string key = "Esto_Se_Supone_Que_Es_Una_Llava_Secreta";
-            var symmetricKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
-            var credentials = new SigningCredentials(symmetricKey, SecurityAlgorithms.HmacSha256Signature);
-            var token = new JwtSecurityToken(
-                    issuer: "Emisor_Admin",
-                    audience: "Receptor",
-                    expires: DateTime.Now.AddMinutes(10),
-                    signingCredentials: credentials
-                );
-            return Ok(new JwtSecurityTokenHandler().WriteToken(token));
+            var token = protoken.Program.generator();
+            return Ok(token);
         }
-
-
     }
 }
---
+++ TokenGenerator/Program.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using Nancy.Json;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TokenGenerator
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var user = new User { UserId = 1, EmailAddress = "fernando@sendpizza.com", FirstName = "Fernando", LastName = "Oliva" };
+            var json = new JavaScriptSerializer().Serialize(user);
+            var issuer = "Machete.com/remitente";
+            var authority = "MacheteMontenegro";
+            //la llave privada debe contener 256 caracteres
+            var privateKey = "J6k2eVCTXDp5b97u6gNH5GaaqHDxCmzz2wv3PRPFRsuW2UavK8LGPRauC4VSeaetKTMtVmVzAC8fh8Psvp8PFybEvpYnULHfRpM8TA2an7GFehrLLvawVJdSRqh2unCnWehhh2SJMMg5bktRRapA8EGSgQUV8TCafqdSEHNWnGXTjjsMEjUpaxcADDNZLSYPMyPSfp6qe5LMcd5S9bXH97KeeMGyZTS2U8gp3LGk2kH4J4F3fsytfpe9H9qKwgjb";
+            var userJson = new JavaScriptSerializer().Serialize(user);
+            var createJwt = await CreateJWTAsync(user, issuer, authority, privateKey, userJson);
+            await Console.Out.WriteLineAsync(createJwt);
+            await Console.In.ReadLineAsync();
+        }
+
+        public static async Task<string> CreateJWTAsync(User user, string issuer, string authority, string symSec, string userJson)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var claims = await CreateClaimsIdentities("Datos Usuario en Json " + userJson);
+
+            var token = tokenHandler.CreateJwtSecurityToken(
+                issuer: issuer,
+                audience: authority,
+                subject: claims,
+                notBefore: DateTime.UtcNow,
+                expires: DateTime.UtcNow.AddDays(1),
+                signingCredentials:
+                new SigningCredentials(
+                    new SymmetricSecurityKey(
+                        Encoding.Default.GetBytes(symSec)),
+                        SecurityAlgorithms.HmacSha256Signature));
+
+            return tokenHandler.WriteToken(token);
+        }
+
+        public static Task<ClaimsIdentity> CreateClaimsIdentities(string userJson)
+        {
+            ClaimsIdentity claimsIdentity = new ClaimsIdentity();
+            claimsIdentity.AddClaim(new Claim(ClaimTypes.UserData, userJson));
+            return Task.FromResult(claimsIdentity);
+        }
+        public class User
+        {
+            public int UserId { get; set; }
+            public string EmailAddress { get; set; }
+            public string FullName { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+        }
+    }
+}
