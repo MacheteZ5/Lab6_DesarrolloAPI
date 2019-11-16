9330c267d736a72db9f10feafddcf1642ab7f342
feroliv4z <feroliv4z@gmail.com>
2019-11-16 00:57:45 -0600

    iniciando merge de serie 1 a master

---
+++ LAB6_1251518_1229918_API/Controllers/TokenController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Web.Script.Serialization;
+using protoken;
+namespace LAB6_1251518_1229918_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult GetToken()
+        {
+            var token = protoken.TokenGenerator.generator("J6k2eVCTXDp5b97u6gNH5GaaqHDxCmzz2wv3PRPFRsuW2UavK8LGPRauC4VSeaetKTMtVmVzAC8fh8Psvp8PFybEvpYnULHfRpM8TA2an7GFehrLLvawVJdSRqh2unCnWehhh2SJMMg5bktRRapA8EGSgQUV8TCafqdSEHNWnGXTjjsMEjUpaxcADDNZLSYPMyPSfp6qe5LMcd5S9bXH97KeeMGyZTS2U8gp3LGk2kH4J4F3fsytfpe9H9qKwgjb");
+            return Ok(token);
+        }
+    }
+}
